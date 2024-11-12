57faa23852a5ee5741cf156ffa55172b598d4d43
Eliana. <147487242+Arteinsana7@users.noreply.github.com>
2024-11-12 05:43:16 +0100

    Jest Config 🔍
    configuration for jest unit tests and tested my component ButtonDetail.

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+module.exports = {
+  // Optionally, add other Jest configuration options here
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"], // Chemin vers votre fichier setupTests.ts
+  testEnvironment: "jest-environment-jsdom", // Par défaut, pour tester le DOM
+};
